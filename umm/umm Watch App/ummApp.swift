adadc7d2ed55822af8cea84b3cf614aed989b489
YouBin <alicee0047@gmail.com>
2025-05-27 10:27:40 +0900

    Initial commit

---
+++ umm/umm Watch App/ummApp.swift
@@ -0,0 +1,17 @@
+//
+//  ummApp.swift
+//  umm Watch App
+//
+//  Created by Youbin on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct umm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
