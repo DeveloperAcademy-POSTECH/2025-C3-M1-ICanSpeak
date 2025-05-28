892b36c9c2d7f264b2433cb77d6e77d7be8a5e8b
minjeongkx <2qqqaa@naver.com>
2025-05-28 15:29:27 +0900

    feat: firstDetectView 구현(회색 원형 배경, Asset에 gray컬러 4종 추가)

---
+++ umm/umm Watch App/FirstDetectView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstDetectView.swift
+//  umm Watch App
+//
+//  Created by MINJEONG on 5/28/25.
+//
+
+import SwiftUI
+
+struct FirstDetectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstDetectView()
+}
