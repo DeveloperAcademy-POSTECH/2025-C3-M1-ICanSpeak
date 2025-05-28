869b5b4f001883615b4e2d1b0f9c44f49ce84f64
minjeongkx <2qqqaa@naver.com>
2025-05-28 15:50:32 +0900

    feat: firstDetectView 구현(회색 원형 배경, Asset에 gray컬러 4종 추가)

--- umm/umm Watch App/FirstDetectView.swift
+++ umm/umm Watch App/FirstDetectView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct FirstDetectView: View {
+    @State private var currentDot = 0
+    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Circle()
+                  .foregroundColor(.gray1)
+                  .frame(width: 223, height: 223)
+            Circle()
+                .foregroundColor(.gray2)
+                  .frame(width: 161, height: 161)
+            Circle()
+                  .foregroundColor(.gray3)
+                  .frame(width: 97, height: 97)
+            
+            VStack {
+                Spacer()
+                HStack(spacing: 8) {
+                    ForEach(0..<3) { index in
+                        Circle()
+                            .fill(currentDot == index ? Color.white : Color.gray4)
+                            .frame(width: 12, height: 12)
+                            .animation(.easeInOut(duration: 0.3), value: currentDot)
+                    }
+                }
+                Spacer()
+            }
+        }
+        .onReceive(timer) { _ in
+            currentDot = (currentDot + 1) % 3
+        }
     }
 }
 
