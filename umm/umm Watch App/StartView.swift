5f4b7fc2765c6a8ffaf76d8f10fed22cd90b3eec
llj72 <85468841+llj72@users.noreply.github.com>
2025-06-02 09:52:51 +0900

    StartView 수정

--- umm/umm Watch App/StartView.swift
+++ umm/umm Watch App/StartView.swift
@@ -2,40 +2,34 @@ import SwiftUI
 
 struct StartView: View {
     @State private var isStarted = false
-    @State private var path: [String] = []
     @StateObject private var soundDetector = SoundDetector()
     @StateObject private var gestureDetector = GestureDetector()
 
     var body: some View {
-        NavigationStack(path: $path) {
             VStack {
+              if isStarted {
+                FirstDetectView()
+              } else {
                 Button(action: {
-                    path.append("FirstDetect")
+                  isStarted = true
                     soundDetector.startListening()
                     gestureDetector.startDetecting()
                 }) {
-                    Text("Start")
-                        .font(Font.custom("SF Compact", size: 20))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
+                    ZStack {
+                        Circle()
+                            .fill(Color("Main Orange"))
+                            .frame(width: 134, height: 134)
+                        Text("Start")
+                            .font(Font.custom("SF Compact", size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
                 }
                 .buttonStyle(PlainButtonStyle())
-                .frame(width: 134, height: 134)
-                .background(
-                    Circle()
-                        .fill(Color("Main Orange"))
-                )
-                .clipShape(Circle())
             }
-            .padding()
-            .navigationDestination(for: String.self) { value in
-                if value == "FirstDetect" {
-                    FirstDetectView()
-                }
             }
         }
     }
-}
 
 #Preview {
     StartView()
