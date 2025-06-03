005f0098ad92e3f4f4863ab73777dca57401a3e9
Ella <eunseolee90917@gmail.com>
2025-06-03 14:32:48 +0900

    폰 안깔리는 문제 우선 수정 및 테스트 필요

---
+++ umm/umm Watch App/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  TabView.swift
+//  umm
+//
+//  Created by Ella's Mac on 6/3/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab = 1
+
+    // 전역 상태 객체 생성 (한 번만 생성)
+    @StateObject private var pauseManager = PauseManager()
+    @StateObject private var soundManager = SoundDetectionManager()
+    @StateObject private var motionManager = MotionManager()
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            // 탭 1: 일시정지 / 종료
+            PauseView()
+                .environmentObject(pauseManager)
+                .environmentObject(soundManager)
+                .environmentObject(motionManager)
+                .tag(0)
+                .tabItem {
+                    Label("탭1", systemImage: "house")
+                }
+
+            // 탭 2: 감지 기능 동작
+            DetectionView()
+                .environmentObject(pauseManager)
+                .environmentObject(soundManager)
+                .environmentObject(motionManager)
+                .tag(1)
+                .tabItem {
+                    Label("탭2", systemImage: "gear")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
