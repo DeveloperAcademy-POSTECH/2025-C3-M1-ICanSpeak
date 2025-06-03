1ad428bb91de1ee10efc184540a4501998d55bb6
llj72 <85468841+llj72@users.noreply.github.com>
2025-06-03 11:52:00 +0900

    startView, PauseView 엘라,메이 뷰와 연결

--- umm/umm Watch App/StartView.swift
+++ umm/umm Watch App/StartView.swift
@@ -2,45 +2,45 @@ import SwiftUI
 import WatchConnectivity
 
 struct StartView: View {
-    var body: some View {
   @State private var isStarted = false
   @State private var tabSelection = 1
   
-  
-  var body: some View {
-    if isStarted {
-      TabView(selection: $tabSelection) {
-        PauseView(soundDetector: SoundDetector(), gestureDetector: GestureDetector(), onExit: {
-          isStarted = false
-        })
-          .tag(0)
-        MainView()
-          .tag(1)
-      }
-      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-      .onAppear {
-        tabSelection = 1
-      }
-    } else {
-      Button(action: {
-        let startTime = Date()
-        WatchSessionManager.shared.sendStartTimeToApp(date: startTime)
-        isStarted = true
-      }) {
-        ZStack {
-          Circle()
-            .fill(Color.ummPrimary)
-            .frame(width: 134, height: 134)
-          Text("Start")
-            .font(.sfbold20)
-            .foregroundColor(.white)
+    var body: some View {
+      if isStarted {
+        TabView(selection: $tabSelection) {
+          PauseView(soundDetector: SoundDetectionManager(), gestureDetector: MotionManager(), onExit: {
+            isStarted = false
+          })
+        
+          HandDetectionSwitcherView()
+            .tag(1)
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .onAppear {
+          tabSelection = 1
+        }
+      } else {
+        Button(action: {
+          let startTime = Date()
+          WatchSessionManager.shared.sendStartTimeToApp(date: startTime)
+          isStarted = true
+        }) {
+          ZStack {
+            Circle()
+              .fill(Color.ummPrimary)
+              .frame(width: 134, height: 134)
+            Text("Start")
+              .font(.sfbold20)
+              .foregroundColor(.white)
+          }
+        }
+        .buttonStyle(PlainButtonStyle())
       }
-      .buttonStyle(PlainButtonStyle())
     }
   }
-}
 
-#Preview {
+
+  
+  #Preview {
     StartView()
-}
+  }
