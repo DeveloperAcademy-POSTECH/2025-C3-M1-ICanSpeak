cb7b89a3dec5a9f5e9186098d3176d57fc3bd2b4
Ella <eunseolee90917@gmail.com>
2025-06-03 11:28:41 +0900

    MainView -> HandDetectionSwitcherView 뷰에서 수정

--- umm/umm Watch App/StartView.swift
+++ umm/umm Watch App/StartView.swift
@@ -8,7 +8,7 @@ struct StartView: View {
     var body: some View {
             VStack {
               if isStarted {
-                MainView()
+                  HandDetectionSwitcherView()
               } else {
                 Button(action: {
                   isStarted = true
