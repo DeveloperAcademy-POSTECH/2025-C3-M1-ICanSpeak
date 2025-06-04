f62e52154136384b61e81bc46f85ff6ab3c0966b
Ella <eunseolee90917@gmail.com>
2025-06-04 10:50:06 +0900

    WatchSessionManager delegate 중복 수정

--- umm/umm/ummApp.swift
+++ umm/umm/ummApp.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 @main
 struct ummApp: App {
+    init() {
+        print("🧪 앱 시작 - PhoneSessionManager 초기화")
+        _ = PhoneSessionManager.shared // ✅ 여기서 초기화 발생
+    }
     var body: some Scene {
         WindowGroup {
             TimeLogView()
-                .onAppear {
-                    let _ = PhoneSessionManager.shared
-                }
         }
     }
 }
